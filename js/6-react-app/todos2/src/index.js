89273619be3a97e119141a51f4c4c74e0a4132ba
Ingimar Erlingsson <inkimar@gmail.com>
2018-10-01 10:59:46 +0200

    all imports are needed

--- js/6-react-app/todos2/src/index.js
+++ js/6-react-app/todos2/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+// import TodoForm from './TodoForm'
 
 ReactDOM.render(<App />, document.getElementById('root'))
